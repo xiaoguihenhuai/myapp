bc0974dc63bd44610d3d8a12cd937cc725336ac6
xiaogui <duyajie05@gmail.com>
2021-07-16 14:43:56 +0900

    delete unuse file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { LoginScreen } from './screens/login';
-import { Test } from './test';
-import {Title} from './title'
+import {createStore} from 'redux';
+
+// redux 中的三个重要的部分：action reducer state (store)
+
 
 function App() {
+  const counterReducer = function(state = {count:1},action:any){
+    console.log( action)
+    switch(action.type){
+      case 'test':
+        return {
+          ...state,count:state.count + 1
+        }
+        default:
+          return state
+    }
+  }
+  
+  const store = createStore(counterReducer)
+  
+  console.log(store)
+
+  store.dispatch({
+    type:'test',
+    payload:{}
+  })
+
   return (
     <div>
-      <Title title='我是title'/>
-      <Test />
       <LoginScreen />
     </div>
   );
